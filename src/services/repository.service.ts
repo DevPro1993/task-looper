a9b96aa28b05f8f6a50bba440dff63f1d41f2306
Apoorva Lele <apoorvalele@Apoorvas-MacBook-Air.local>
2022-02-06 16:57:46 +0530

    Code refactoring

--- src/services/repository.service.ts
+++ src/services/repository.service.ts
@@ -21,32 +21,29 @@ export class RepositoryService {
   }
   
   async markAsComplete(task: Task) {
-    const nextTaskId = await this.findNextTaskId(task);
-    await this.putDocument('currentTaskId', nextTaskId);
+    const nextTask = await this.findNextTask(task);
+    const prevTask = await this.findPrevTask(task);
     if (task.currentCount < task.maxRepeatCount) {
       task.currentCount++;
       await this.patchDocument(`tasks/${task.id}`, task);
     } else {
-      let prevTask = await this.findPrevTask(task);
-      if (!prevTask) {
-        await this.putDocument('head', nextTaskId);
-      } else {
-        if (!task.nextTaskId) {
-          prevTask.nextTaskId = null;
-        } else {
-          prevTask.nextTaskId = task.nextTaskId;
-        }
-        await this.putDocument(`tasks/${task.id}`, prevTask);
-      }
+      console.log(nextTask);
       await this.deleteDocument(`tasks/${task.id}`);
+      if (!prevTask) {
+        await this.postDocument('head', nextTask?.id ?? null)
+      } else {
+        prevTask.nextTaskId = task.nextTaskId;
+        await this.patchDocument(`tasks/${task.id}`, task)
+      }
     }
+    await this.postDocument('currentTaskId', nextTask?.id ?? await this.getDocument('head'))
   }
 
   async addNewTask(task: Task) {
     let head = await this.getDocument('head');
     task.nextTaskId = head ?? null;
-    await this.putDocument('head', <string>task.id)
-    await this.putDocument(`tasks/${task.id}`, task);
+    await this.postDocument('head', <string>task.id)
+    await this.postDocument(`tasks/${task.id}`, task);
   }
 
 
@@ -60,18 +57,19 @@ export class RepositoryService {
 
 
   private async findPrevTask(task: Task): Promise<Task | null> {
+    debugger;
     let headTask = await this.getTaskById(await this.getDocument('head'));
     if (headTask.id === task.id) return null;
     let currTask = headTask;
-    while (headTask.nextTaskId !== task.id) {
+    while (currTask.nextTaskId !== task.id) {
       currTask = await this.getTaskById(currTask.nextTaskId as string);
     }
     return currTask;
   }
 
-  private async findNextTaskId(task: Task): Promise<string | null> {
-    if (!task.nextTaskId && await this.getDocument('head') === task.id) return null;
-    return task.nextTaskId ?? await this.getDocument('head');
+  private async findNextTask(task: Task): Promise<Task | null> {
+    if (!task.nextTaskId) return null;
+    return await this.getTaskById(task.nextTaskId);
   }
 
 
@@ -80,7 +78,7 @@ export class RepositoryService {
     return await result.json();
   }
 
-  private putDocument(name: string, data: any) {
+  private postDocument(name: string, data: any) {
     return fetch(this.url + name + '.json', {
       method: 'PUT',
       headers: {
